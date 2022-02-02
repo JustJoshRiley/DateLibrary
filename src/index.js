cbf01d38c896abf86b1fab1e77fe88280f0d1d16
justjoshriley <joshriley316@gmail.com>
2022-02-02 14:54:13 -0800

    updated and finished challenge 02

--- src/index.js
+++ src/index.js
@@ -1,37 +1,65 @@
-// Challenge 1
-class D {
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const mon = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const dy = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+class dateObject {
     constructor(...args) {
         this._date = new Date(...args)
     }
+    // Year
     get year() {
         return this._date.getFullYear()
     }
-    get day() {
-        return this._date.getDay()
+    get yr() {
+        return parseInt(this._date.getFullYear().toString().slice(2))
     }
+
+    // Month
+    get month() {
+        return months[this._date.getMonth()]
+    }
+    get mon() {
+        return mon[this._date.getMonth()]
+    }
+
+    // Day
+    get day() {
+        return days[this._date.getDay()]
+    }
+    get dy() {
+        return dy[this._date.getDay()]
+    }
+
+
+
     get date() {
         return this._date.getDate()
     }
-    get month() {
-        return this._date.getMonth()
-    }
+
+
     get hours() {
         return this._date.getHours()
     }
     get min() {
         return this._date.getMinutes()
     }
+    get secs() {
+        return this._date.getSeconds()
+    }
     // Challenge 2
 
 }
 
-const myBirthday = new D('1998', '10', '14')
-const today = new D()
+const myBirthday = new dateObject('1998', '10', '14')
 // console.log( myBirthday._date.getDay() )
 // console.log( myBirthday._date.getFullYear() )
 console.log( myBirthday.year)
-console.log( myBirthday.day)
-console.log( myBirthday.date)
+console.log( myBirthday.yr)
+// console.log( myBirthday.date)
 console.log( myBirthday.month)
-console.log( myBirthday.hours)
-console.log( myBirthday.min)
+console.log( myBirthday.mon)
+console.log( myBirthday.day)
+console.log( myBirthday.dy)
+// console.log( myBirthday.hours)
+// console.log( myBirthday.min)
