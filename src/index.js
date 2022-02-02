6d56ed89e18cf9cdb301d5ef3298b9c2780f5ca3
justjoshriley <joshriley316@gmail.com>
2022-02-02 12:05:32 -0800

    finished challenge 02

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ class D {
     constructor(...args) {
         this._date = new Date(...args)
     }
+    get year() {
+        return this._date.getFullYear()
+    }
+    get day() {
+        return this._date.getDay()
+    }
+    get date() {
+        return this._date.getDate()
+    }
+    get month() {
+        return this._date.getMonth()
+    }
+    get hours() {
+        return this._date.getHours()
+    }
+    get min() {
+        return this._date.getMinutes()
+    }
     // Challenge 2
 
 }
 
 const myBirthday = new D('1998', '10', '14')
 const today = new D()
-// console.log(myBirthday)
-// console.log(today)
-console.log( myBirthday._date.getDay() )
+// console.log( myBirthday._date.getDay() )
+// console.log( myBirthday._date.getFullYear() )
+console.log( myBirthday.year)
+console.log( myBirthday.day)
+console.log( myBirthday.date)
+console.log( myBirthday.month)
+console.log( myBirthday.hours)
+console.log( myBirthday.min)
